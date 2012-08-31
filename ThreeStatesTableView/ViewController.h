96d46fae9987a7ca854c6711ff52ce42e330e3db
TZ <toza@localhost.(none)>
2012-08-31 16:39:02 +0100

    Initial Commit

---
+++ ThreeStatesTableView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ThreeStatesTableView
+//
+//  Created by TZ on 31/08/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
